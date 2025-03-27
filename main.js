0fd0321849bce6b062df37415de3d457584c6c0c
CarmeloMelito <melitocarmelo211@gmail.com>
2025-03-27 09:34:50 +0100

    feat:create server postman

--- main.js
+++ main.js
@@ -4,4 +4,52 @@ const express = require('express');
 const app=express();
 const port=4000;
 
-console.log(express);
+//console.log(express);
+app.use(express.static('public'));
+  const post=[
+    {
+        titolo: "Ciambellone soffice",
+        contenuto: "Una ricetta deliziosa per un ciambellone soffice e profumato.",
+        images: "ciambellone.jpeg",
+        tag: ["dolci",  "ciambella"]
+    },
+    {
+        titolo: "Cracker di barbabietola",
+        contenuto: "Snack croccanti e salutari a base di barbabietola.",
+        images: "cracker_barbabietola.jpeg",
+        tag: ["cracker", "barbabietola"]
+    },
+    {
+        titolo: "Pane Fritto Dolce",
+        contenuto: "Pane.",
+        images: "pane_fritto_dolce.jpeg",
+        tag: ["pane", "fritto", "dolce"]
+    },
+    {
+        titolo: "Pasta alla barbabietola",
+        contenuto: "pasta alla barbabietola",
+        images: "pasta_barbabietola.jpeg",
+        tag: ["pasta", "vegetariano", "barbabietola"]
+    },
+    {
+        titolo: "Torta Paesana",
+        contenuto: "Torta",
+        images: "torta_paesana.jpeg",
+        tag: ["torta", "paesana", "noci"]
+    }
+  ]
+
+  app.get('/', (req, res) => {
+    res.send('Ricette');
+});
+
+
+app.get('/bacheca', (req, res) => {
+    res.json(post);
+});
+
+
+//Porta Server
+  app.listen(port, () => {
+    console.log(`Server port ${port}`);
+  });
